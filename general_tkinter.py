bffe6d5662a40ff09561e4b7f65ecd596478768a
VITALY CHU <vitaliy69@mail.ru>
2021-09-29 10:44:22 +0300

    Этот же пример через ООП

--- general_tkinter.py
+++ general_tkinter.py
@@ -3,4 +3,30 @@
 
 from tkinter import *
 
+
+# def str_to_sort_list(event):
+#     s = ent.get()
+#     s = s.split()
+#     s.sort()
+#     lab['text'] = ' '.join(s)
+
+
 root = Tk()
+root.geometry("800x500+1000+300")
+root.title("Tkinter начало")
+
+# ent = Entry(root, width=20)
+# btn = Button(root, text="Преобразовать")
+# lab = Label(root, width=20, bg='#000', fg='white')
+#
+# btn.bind('<Button-1>', str_to_sort_list)
+# ent.pack()
+# btn.pack()
+# lab.pack()
+
+# Этот же пример через ООП
+
+
+
+
+root.mainloop()
