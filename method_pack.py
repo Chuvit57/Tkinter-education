234c45cb82ac802924c1dddecc0a7416d59feb13
VITALY CHU <vitaliy69@mail.ru>
2021-09-29 23:37:15 +0300

    Расположение снизу: method pack()

--- method_pack.py
+++ method_pack.py
@@ -9,20 +9,22 @@ root.geometry("900x1000+1000+300")
 root['bg'] = 'lightblue'
 root.title("Widgets")
 
-
-# Расположение по центру
 lab1 = Label(width=17, height=8, bg="yellow", text="1", font=16, fg="black")
 lab2 = Label(width=17, height=8, bg="orange", text="2", font=16, fg="white")
 lab3 = Label(width=17, height=8, bg="green", text="3", font=16, fg="white")
 lab4 = Label(width=17, height=8, bg="blue", text="4", font=16, fg="white")
 
-lab1.pack()
-lab2.pack()
-lab3.pack()
-lab4.pack()
+# Расположение по центру
+# lab1.pack()
+# lab2.pack()
+# lab3.pack()
+# lab4.pack()
 
-
-
+# Расположение снизу
+lab1.pack(side=BOTTOM)
+lab2.pack(side=BOTTOM)
+lab3.pack(side=BOTTOM)
+lab4.pack(side=BOTTOM)
 
 
 root.mainloop()
