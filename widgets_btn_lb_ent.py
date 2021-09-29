91e0bca7711f8f591c6021255a507468284f94a5
VITALY CHU <vitaliy69@mail.ru>
2021-09-29 19:30:01 +0300

    Виджет: Label без переменной

--- widgets_btn_lb_ent.py
+++ widgets_btn_lb_ent.py
@@ -13,6 +13,9 @@ from tkinter import *
 #     btn['fg'] = '#ffffff'
 #     btn['activeforeground'] = '#ffffff'
 
+def take():
+    lab3['text'] = "Выдано"
+
 
 root = Tk()
 
@@ -22,5 +25,23 @@ root.title("Widgets")
 
 # btn = Button(text='Изменить', width=15, height=5, command=change)
 # btn.pack()
+
+
+# Label - метка
+# lab1 = Label(text='Машинное обучение', font="Arial 32")
+# lab2 = Label(text='Распознавание образов', font=("Comic Sans MS", 24, "bold"))
+# lab1.config(bd=20, bg='#ffaaaa')
+# lab2.config(bd=20, bg='#aaffff')
+
+# Label без переменной
+Label(text="Пункт выдачи").pack()
+Button(text="Взять", command=take).pack()
+
+lab3 = Label(width=10, height=1)
+lab3.pack()
+
+
+# lab1.pack()
+# lab2.pack()
 
 root.mainloop()
