5c6a059469878d2815cc6ec55216f30b68c842ed
VITALY CHU <vitaliy69@mail.ru>
2021-09-29 15:24:34 +0300

    Виджет: Button

--- widgets_btn_lb_ent.py
+++ widgets_btn_lb_ent.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*
+
+from tkinter import *
+
+
+# Button
+
+# def change():
+#     btn['text'] = 'Изменено'
+#     btn['bg'] = '#000'
+#     btn['activebackground'] = '#555555'
+#     btn['fg'] = '#ffffff'
+#     btn['activeforeground'] = '#ffffff'
+
+
+root = Tk()
+
+root.geometry("700x500+1000+300")
+root['bg'] = 'lightblue'
+root.title("Widgets")
+
+# btn = Button(text='Изменить', width=15, height=5, command=change)
+# btn.pack()
+
+root.mainloop()
