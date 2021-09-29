12e87192b08f05a35bc208a869a9171fecf26e65
VITALY CHU <vitaliy69@mail.ru>
2021-09-30 00:00:52 +0300

    Расположение во фреймах(Рамках): method pack()

--- method_pack.py
+++ method_pack.py
@@ -5,14 +5,18 @@ from tkinter import *
 
 root = Tk()
 
-root.geometry("900x1000+1000+300")
-root['bg'] = 'lightblue'
-root.title("Widgets")
+# root.geometry("900x1000+1000+300")
+# root['bg'] = 'lightblue'
+root.title("method pack()")
 
-lab1 = Label(width=17, height=8, bg="yellow", text="1", font=16, fg="black")
-lab2 = Label(width=17, height=8, bg="orange", text="2", font=16, fg="white")
-lab3 = Label(width=17, height=8, bg="green", text="3", font=16, fg="white")
-lab4 = Label(width=17, height=8, bg="blue", text="4", font=16, fg="white")
+# Расположение во фреймах
+f_top = Frame(root)
+f_bot = Frame(root)
+
+lab1 = Label(f_top, width=17, height=8, bg="yellow", text="1", font=16, fg="black")
+lab2 = Label(f_top, width=17, height=8, bg="orange", text="2", font=16, fg="white")
+lab3 = Label(f_bot, width=17, height=8, bg="green", text="3", font=16, fg="white")
+lab4 = Label(f_bot, width=17, height=8, bg="blue", text="4", font=16, fg="white")
 
 # Расположение по центру
 # lab1.pack()
@@ -28,10 +32,19 @@ lab4 = Label(width=17, height=8, bg="blue", text="4", font=16, fg="white")
 
 # Расположение слева по центру
 
+# lab1.pack(side=LEFT)
+# lab2.pack(side=LEFT)
+# lab3.pack(side=LEFT)
+# lab4.pack(side=LEFT)
+
+# Расположение во фреймах
+f_top.pack()
+f_bot.pack()
 lab1.pack(side=LEFT)
 lab2.pack(side=LEFT)
 lab3.pack(side=LEFT)
 lab4.pack(side=LEFT)
+
 
 
 root.mainloop()
